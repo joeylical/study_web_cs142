9bd24d110cc17d9534570a4b8d9aa51b6e554eaa
Li Jiayi <Li.Jiayi@edu.sait.ca>
2024-02-15 10:09:15 -0700

    solve eslint problems

--- projects/project4/components/States/index.jsx
+++ projects/project4/components/States/index.jsx
@@ -17,20 +17,23 @@ class States extends React.Component {
       leading: '',
       states: window.cs142models.statesModel(),
       selectedStates: window.cs142models.statesModel(),
-    }
+    };
   }
 
   updateLeading(event) {
     const leading = event.target.value;
     let r = this.state.states.filter(s => s.toLowerCase().startsWith(leading));
-    if (r.length == 0) {
-      r = [<i>No Matches</i>];
+    if (r.length === 0) {
+      r = [(
+        <i key='nomatch'>No Matches</i>
+      )];
     }
     this.setState({ leading:  leading, selectedStates: r});
   }
 
   render() {
-    return <div>
+    return (
+    <div>
       <label>State Name:</label>
       <input
         type="text"
@@ -38,9 +41,10 @@ class States extends React.Component {
         onChange={(e) => this.updateLeading(e)}
       />
       <ul >
-      {this.state.selectedStates.map(s=><li>{s}</li>)}
+      {this.state.selectedStates.map(s=><li key={s}>{s}</li>)}
       </ul>
-    </div>;
+    </div>
+    );
   }
 }
 
